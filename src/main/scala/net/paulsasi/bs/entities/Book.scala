8447de82b63645122ca04c0bdf34e41cec03e203
ethel <paulsasieta.upv@gmail.com>
2022-06-06 22:10:11 +0200

    Book initial impl and testing

--- src/main/scala/net/paulsasi/bs/entities/Book.scala
+++ src/main/scala/net/paulsasi/bs/entities/Book.scala
@@ -1,5 +1,13 @@
 package net.paulsasi.bs.entities
 
-class Book {
+import net.paulsasi.bs.utils.Date
+
+class Book(idc: Long, namec: String, authorc: Author, releaseDatec: Date, topicc: Topics.Topic) {
+
+  val id: Long = idc
+  val name: String = namec
+  val author: Author = authorc
+  val releaseDate: Date = releaseDatec
+  val topic: Topics.Topic = topicc
 
 }
--- src/test/scala/net/paulsasi/bs/entities/BookTest.scala
+++ src/test/scala/net/paulsasi/bs/entities/BookTest.scala
@@ -1,5 +1,26 @@
-package net.paulsasi.bs.entities
+import org.scalatest.funsuite.AnyFunSuite
 
-class BookTest {
+import net.paulsasi.bs.entities.Book
+import net.paulsasi.bs.entities.Author
+import net.paulsasi.bs.entities.Topics
+import net.paulsasi.bs.utils.Date
 
+import java.time.LocalDate
+
+class BookTest extends AnyFunSuite {
+  test("Book.constructor") {
+    val id = 1
+    val name = "Book Name"
+    val author = Author(1, "Name", "Surname", "Spain")
+    val release = Date(LocalDate.of(2021, 10, 10))
+    val topic = Topics.HORROR
+    val book = Book(id, name, author, release, topic)
+    assert(
+              book.id == 1 &&
+              book.name == name &&
+              book.author == author &&
+              book.releaseDate == release &&
+              book.topic == topic
+    )
+  }
 }
